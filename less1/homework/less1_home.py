4d1a4a10661016ab60d280ae4d1997dc572ed0d0
CrUeLGeRoI <elbg2016@gmail.com>
2021-12-11 17:49:59 +0200

    Less2 and added homework for less1

---
+++ less1/homework/less1_home.py
@@ -0,0 +1,12 @@
+# constants
+a = 10
+b = 20
+
+# arithmetical actions
+subtraction = a - b
+summary = a + b
+division = a / b
+multiplication = a * b
+degree = a ** b
+# output
+print("Hello world. Values:", subtraction, summary, division, multiplication, degree)
---
+++ less2/less2.py
@@ -0,0 +1,6 @@
+name = "HIJFRBHIDFG"
+print(f'\nHello, {name}')
+a = [1, 2, 3, 4, 5, 6, 7]
+b = [8, 9, 10, 11, 12, 13]
+c = [14, 15, 16, 17, 18, 19]
+print(set(f"{a}"))
