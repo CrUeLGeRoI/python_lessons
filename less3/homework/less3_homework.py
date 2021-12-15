bdae1ef255840b0c7ef1be663549db183ba4d60e
CrUeLGeRoI <elbg2016@gmail.com>
2021-12-15 20:46:18 +0200

    Bug fix

--- less3/homework/less3_homework.py
+++ less3/homework/less3_homework.py
@@ -1,6 +1,19 @@
-a = int(input("Enter num: "))
-b = int(input("Enter 2nd num: "))
-if a % b == 0:
-    print("Yes")
-else:
-    print("No")
+# a = int(input("Enter num: "))
+# b = int(input("Enter 2nd num: "))
+# if a % b == 0:
+#     print("Yes")
+# else:
+#     print("No")
+import math
+
+x = int(input("Enter num: "))
+
+if math.fabs(x) < 2:
+    y = x - math.exp(x)
+    print(y)
+elif x <= -2:
+    y = math.log(math.pow(x, 2))
+    print(y)
+elif x >= 2:
+    y = math.pow(math.sin(x), 2)
+    print(y)
