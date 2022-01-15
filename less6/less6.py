7ffc1752f9262de3674fc251dbc947e6bf7ffc65
CrUeLGeRoI <elbg2016@gmail.com>
2022-01-15 19:59:05 +0200

    less6 and less7 and their homeworks

---
+++ less6/less6.py
@@ -0,0 +1,25 @@
+# Skier
+# a = 1
+# b = 10
+# while b < 20:
+#     b += b * 0.1
+#     a += 1
+# print(f"Нужное кол-во дней:{a}")
+# y = x2
+# x = 1
+# while x <= 10:
+#     y = x ** 2
+#     print(f"x: {x} y: {y}\n")
+#     x += 1
+# import time
+#
+# s = 0  # Start value of sum
+# x = 1
+# print("S = 1 + 3 + 5 + 7 + 9 +.... + n")
+# time.sleep(1)
+# n = int(input("n: "))
+# while x <= n + 1:
+#     s += x
+#     x += 2
+# print(f"Sum of {s} value {x - 1}")
+
