06e7a8c6145715d2daa82fe0dd3b24cb6805b6f9
CrUeLGeRoI <elbg2016@gmail.com>
2022-01-26 17:09:53 +0200

    added less4_homework.py and inserted all home tasks from less4.py and tweak to dates_of_lessons.txt, also less 9

---
+++ less9/less9.py
@@ -0,0 +1,12 @@
+# car = ['honda', 'lamborghini', 'mazda']
+# # car.sort(reverse=True)
+# print(sorted(car))
+
+pizza = ["Куриная", "Мясной микс", "С сырным бортиком"]
+for i in pizza:
+    print(f"Я хочу заказать пиццу '{i}' ")
+print("Я обожаю пиццу!")
+
+
+
+
