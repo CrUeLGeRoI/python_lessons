30ad44f7b7222393ebabdc8cc5e9dad3f046f488
CrUeLGeRoI <elbg2016@gmail.com>
2022-02-12 18:20:30 +0200

    less14.py and less14_homework.py

---
+++ less14/less14.py
@@ -0,0 +1,7 @@
+def message(name, age):
+    print(f"Hello, {name}! Your age is: {age}")
+
+
+message("Gleb", 10)
+message("Arina", 13)
+message("Arina", 799)
---
+++ less14/less14_homework.py
@@ -0,0 +1,41 @@
+# 1
+# def display_message():
+#     print("Данное сообщение выведено с помощью функции!")
+#
+#
+# display_message()
+
+# 2
+# def favorite_book(title):
+#     print(f"One of my favourite books is {title}")
+#
+#
+# # Alice
+# favorite_book(input("Title of favorite book:"))
+
+
+# 3 and 4
+# def make_shirt(sixe= "L", title= "I love Python"):
+#     if sixe == "" and title == "":
+#         sixe = "L"
+#         title = "I love Python"
+#     shirt = {"size": sixe, "titlle": title}
+#     return shirt
+#
+#
+# a = make_shirt(58, "Mur, mur, mur, Timur!")
+# print(f"Size of shirt is: {a['size']}. Title of shirt is: {a['titlle']}")
+# a = make_shirt(title="Mur, mur, mur, Timur!", sixe=56)
+# print(f"Size of shirt is: {a['size']}. Title of shirt is: {a['titlle']}")
+# a = make_shirt()
+# print(f"Size of shirt is: {a['size']}. Title of shirt is: {a['titlle']}")
+
+# a
+# 5
+# def describe_city(city, country="Mars"):
+#     print(f"{city} is in {country}")
+#
+#
+# describe_city("Kharkov", "Ukraine")
+# describe_city("Moskva", "Russia")
+# describe_city("UFO")
